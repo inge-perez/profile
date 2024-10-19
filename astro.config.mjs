be9ae93001cf9fda771cb72101ffb9babdb68417
jsperez <jsperez@veritran.com>
2024-10-19 16:33:57 -0500

    git actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,13 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://devolio.devaradise.com',
   integrations: [mdx(), sitemap(), tailwind(), partytown()],
   markdown: {
     extendDefaultPlugins: true,
     rehypePlugins: [[autoNewTabExternalLinks, {
       domain: 'localhost:4321'
     }]]
-  }
+  },
+  site: 'https://inge-jsperez.github.io',
+  base: 'my-repo',
 });
